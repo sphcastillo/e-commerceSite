c7dbee6325946501aa8b5e265673eb4c750fe335
Sophia Castillo <Sphcastillo@gmail.com>
2021-04-19 16:21:21 -0700

    can successfully delete items from cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { addToCart } from '../actions/cartActions';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 import MessageBox from '../components/MessageBox';
 
 import product1 from '../images/product1.jpeg';
@@ -39,7 +39,7 @@ export default function CartScreen(props) {
     }, [dispatch, productId, qty]);
 
     const removeFromCartHandler = (id) => {
-        // delete action
+        dispatch(removeFromCart(id));
     }
 
     const checkoutHandler = () => {
