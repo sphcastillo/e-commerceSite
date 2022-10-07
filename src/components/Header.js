cafa7c279115437f40bedb6e7075e2040491379f
Sophia Castillo <Sphcastillo@gmail.com>
2022-10-07 12:08:45 -0700

    added globalstyles

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,8 +23,26 @@ function Header() {
                 
                 {/* Search */}
                 <div className='hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500'>
-                    <input className="p-2 h-full w-6 flex-grow" type="text" />
+                    <input className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" type="text" />
                     <SearchIcon className='h-12 p-4' />
+                </div>
+
+                {/* Right */}
+                <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
+                    <div className="link">
+                        <p>Hello Sophia Castillo</p>
+                        <p className="font-extrabold md:text-sm">Account & Lists</p>
+                    </div>
+
+                    <div className="link">
+                        <p>Returns</p>
+                        <p className="font-extrabold md:text-sm">& Orders</p>
+                    </div>
+
+                    <div className="link">
+                        <ShoppingCartIcon className='h-10' />
+                        <p className="font-extrabold md:text-sm" >Basket</p>
+                    </div>
                 </div>
             </div>
 
