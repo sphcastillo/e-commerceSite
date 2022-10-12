70392577aab2be256f768aa73a232b13a9c168f0
Sophia Castillo <Sphcastillo@gmail.com>
2022-10-12 11:33:33 -0700

    Rendering Products from FAKE STORE API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
-import Header from "../components/Header";                   
+import Header from "../components/Header";  
+import ProductFeed from "../components/ProductFeed";                
 
-export default function Home() {
+export default function Home({ products }) {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
@@ -12,10 +13,31 @@ export default function Home() {
       <Header />
 
       <main className="max-w-screen-2xl mx-auto">
-        {/* Banner */}
+
         <Banner />
-        {/* Product Feed */}
+
+        <ProductFeed products={products} />
       </main>
     </div>
   );
 }
+
+// by having this function... this tells NEXT.JS /// 
+// this is no longer a static page
+// needs to have the middle server step
+// calculate something on the server first, render out the page and then send it to the browser
+
+export async function getServerSideProps(context){
+
+  const products = await fetch('https://fakestoreapi.com/products').then(
+      (res) => res.json()
+  );
+
+  return { 
+      props: {
+          products,
+      }
+  }
+}
+
+// GET >>> https://fakestoreapi.com/products
