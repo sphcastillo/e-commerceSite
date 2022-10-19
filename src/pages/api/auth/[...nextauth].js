d0d70f76dfb9f5aa5b189011b82430da9d2fad4c
Sophia Castillo <Sphcastillo@gmail.com>
2022-10-19 02:02:33 -0700

    new variables

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,14 +5,15 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            secret: process.env.SECRET,
         }),
-        // FacebookProvider({
-        //     clientId: process.env.FACEBOOK_CLIENT_ID,
-        //     clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-            
-        // })
+        FacebookProvider({
+            clientId: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+            secret: process.env.SECRET,
+        })
     ]
 });
 
