68846e5cab234461b5ce2e1b72bd141977fbaea8
Sophia Castillo <Sphcastillo@gmail.com>
2022-10-19 04:17:09 -0700

    forgot to change EV on Vercel

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 
 export default NextAuth({
+    // Configure one or more authentication providers
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
